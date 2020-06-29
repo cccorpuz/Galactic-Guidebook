002fbdc93b675983733f7b3665fad3451b38bc56
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-06-29 12:36:34 -0500

    First Commit

---
+++ Galactic-Guidebook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Galactic-Guidebook
+//
+//  Created by Crispin Corpuz on 6/29/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Galactic-Guidebook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Galactic-Guidebook
+//
+//  Created by Crispin Corpuz on 6/29/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
