32d8c72e14514ddd41d8d194837f46036a49cf63
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-07-06 00:16:13 -0500

    Tried adding CollectionView

--- Galactic-Guidebook/SecondViewController.swift
+++ Galactic-Guidebook/SecondViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var planetCollectionView: UICollectionView!
+    
+    var selectedPlanet : Planet?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        planetCollectionView.delegate = self
+        planetCollectionView.dataSource = self
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let object = Planet(t: "Earth", desc: "It's green and we live on it", l: "https://space-facts.com", im: UIImage(named: "earth")!)
+        planets.append(object)
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return planets.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = planetCollectionView.dequeueReusableCell(withReuseIdentifier: "collectionID", for: indexPath)
+        return cell
+    }
+    
 }
 
